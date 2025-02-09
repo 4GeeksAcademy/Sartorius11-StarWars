707233d58f1bd21a5570e789ac4bd0675b25e8eb
Fernando Sartorius <92523759+Sartorius11@users.noreply.github.com>
2025-02-09 18:56:39 +0000

    ContanctList

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -2,53 +2,91 @@ const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
 			message: null,
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			contacts: [],
+			currentContacts: {}
+
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
 
-			getMessage: async () => {
-				try{
-					// fetching data from the backend
-					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
-					const data = await resp.json()
-					setStore({ message: data.message })
-					// don't forget to return something, that is how the async resolves
-					return data;
-				}catch(error){
-					console.log("Error loading message from backend", error)
+			setCurrentContact: (contact) => { setStore({ currentContacts: contact }) },
+
+			getContacts: async () => {
+				const host = 'https://playground.4geeks.com/contact';
+				const user = 'Sartorius11';
+
+				const uri = `${host}/agendas/${user}/contacts`;
+				const options = {
+					method: 'GET'
 				}
+				const response = await fetch(uri, options);
+				if (!response.ok) {
+					console.error('Error', response.status, response.statusText);
+					return;
+				}
+				const data = await response.json();
+
+				setStore({ contacts: data.contacts })
 			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+			addContacts: async (dataToSend) => {
+				const host = 'https://playground.4geeks.com/contact';
+				const user = 'Sartorius11';
+				const uri = `${host}/agendas/${user}/contacts`;
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
+				const options = {
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/json'
+					},
+					body: JSON.stringify(dataToSend)
+				}
+				const response = await fetch(uri, options);
+				if (!response.ok) {
+					console.error('Error', response.status, response.statusText);
+					return;
+				}
+				getActions().getContacts();
 
-				//reset the global store
-				setStore({ demo: demo });
+			},
+
+			deleteContact: async (id) => {
+				const host = 'https://playground.4geeks.com/contact';
+				const user = 'Sartorius11';
+				const uri = `${host}/agendas/${user}/contacts/${id}`;
+				const options = { method: "DELETE" };
+				const response = await fetch(uri, options);
+				if (!response.ok) {
+					console.error('Error', response.status, response.statusText);
+					return;
+				}
+
+				// Actualizar la lista después de eliminar
+				getActions().getContacts();
+			},
+			updateContact: async (contact, id) => {
+				// PUT Method
+				const host = 'https://playground.4geeks.com/contact';
+				const user = 'Sartorius11';
+				const dataToSend = contact;
+				const uri = `${host}/agendas/${user}/contacts/${id}`;
+				const options = {
+					method: "PUT",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify(dataToSend),
+				};
+				const response = await fetch( uri, options );
+				if (!response.ok) {
+					console.log("Error:", response.status, response.statusText);
+					return;
+				}
+				getActions().getContacts();
+				setStore({ currentContacts: {} });
 			}
+
 		}
 	};
 };
+
 
 export default getState;
