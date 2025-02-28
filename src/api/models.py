21ed7f05e5d7779670e0ca6e285f89d0a29a1539
Fernando Sartorius <sartoxr@gmail.com>
2025-02-28 17:37:03 +0000

    modelos

--- src/api/models.py
+++ src/api/models.py
@@ -18,3 +18,73 @@ class Users(db.Model):
         return {"id": self.id,
                 "email": self.email,
                 "is_active": self.is_active}
+
+
+class Followers(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    following_id = db.Column(db.Integer)
+    following_id = db.Column(db.Integer)
+
+
+class Coments (db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column (db.String(100), unique = False, nullable = True)
+    user_id = db.Column(db.Integer)
+    post_id = db.Column(db.Integer)
+
+
+class Medias (db.Model):
+   id = db.Column(db.Integer, primary_key=True)
+   type = db.Column(db.String(40),unique = False ,nullable = True)
+   url = db.Column(db.String(100),unique = True ,nullable = False)
+   post_id = db.Column(db.Integer)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(40),unique = False ,nullable = True)
+    description = db.Column(db.String(200), unique = False ,nullable = True)
+    body = db.Column (db.String(100), unique = False, nullable = True)
+    date = db.Column (db.String, unique = True, nullable = True)
+    image_url = db.Column (db.String(100), unique = False, nullable = True)
+    user_id = db.Column(db.Integer)
+
+
+class  charactersFavorites(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    character_id = db.Column(db.Integer)
+
+
+class character(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column (db.String(50), unique = False, nullable = True)
+    height = db.Column (db.String(100), unique = False, nullable = True)
+    mass = db.Column (db.String(20), unique = False, nullable = True)
+    hair_color = db.Column (db.String(10), unique = False, nullable = True)
+    skin_color = db.Column (db.String(30), unique = False, nullable = True)
+    birth_year = db.Column (db.String(30), unique = False, nullable = True)
+    gender = db.Column (db.String(10), unique = False, nullable = True)
+
+
+class  planetFavorites (d.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    planet_id = db.Column(db.Integer)
+
+
+class planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column (db.String(100), unique = True, nullable = False)
+    diameter = db.Column (db.String(20), unique = False, nullable = False)
+    rotation_period = db.Column (db.String(10), unique = True, nullable = False)
+    orbital_period = db.Column (db.String(10), unique = True, nullable = False)
+    gravity = db.Column (db.String(30), unique = False, nullable = False)
+
+
+
+
+
+
+
+
