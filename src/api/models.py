59b98cf619fe319ecbd5120c78eb18d4fcd53864
Fernando Sartorius <92523759+Sartorius11@users.noreply.github.com>
2025-03-05 20:30:37 +0000

    getsasdf

--- src/api/models.py
+++ src/api/models.py
@@ -24,12 +24,30 @@ class Users(db.Model):
                 'first_name': self.first_name,
                 'last_name': self.last_name}
 
+class Products(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), unique=True, nullable=False)
+    description = db.Column(db.String())
+    price = db.Column(db.Float, nullable=False)
+
+    def __repr__(self):
+        return f'<Product: {self.id} - {self.name}>'
+
+    def serialize(self):
+        return {'id': self.id,
+                'name': self.name,
+                'description': self.description,
+                'price': self.price}
+
+
 class Followers(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     following_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     following_to = db.relationship('Users', foreign_keys=[following_id], backref=db.backref('following_to'))
     follower_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     follower_to = db.relationship('Users', foreign_keys=[follower_id], backref=db.backref('follower_to'))
+
+    
     
 class Comments(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -56,12 +74,22 @@ class Post(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     user_to = db.relationship('Users', backref=db.backref('posts'))
 
+
 class CharactersFavorites(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     character_id = db.Column(db.Integer, db.ForeignKey('characters.id'))
     user_to = db.relationship('Users', backref=db.backref('characters_favorites'))
     character_to = db.relationship('Characters', backref=db.backref('favorited_by'))
+    def __repr__(self):
+        return f'<CharactersFavorite: {self.id} - {self.character_id}>'
+
+    def serialize(self):
+        return {'id': self.id,
+                'character_id': self.character_id,
+                'user_id': self.user_id}
+
+
 
 class Characters(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -72,6 +100,19 @@ class Characters(db.Model):
     skin_color = db.Column(db.String(30), nullable=True)
     birth_year = db.Column(db.String(30), nullable=True)
     gender = db.Column(db.String(10), nullable=True)
+    def __repr__(self):
+        return f'<Character: {self.id} - {self.name}>'
+
+    def serialize(self):
+        return {'id': self.id,
+                'name': self.name,
+                'height': self.height,
+                'mass': self.mass,
+                'skin color': self.skin_color,
+                'birth date': self.birth_year,
+                'gender': self.gender}
+
+
 
 class PlanetFavorites(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -79,6 +120,14 @@ class PlanetFavorites(db.Model):
     planet_id = db.Column(db.Integer, db.ForeignKey('planets.id'))
     user_to = db.relationship('Users', backref=db.backref('planet_favorites'))
     planet_to = db.relationship('Planets', backref=db.backref('favorited_by'))
+    def __repr__(self):
+        return f'<PlanetFavorite: {self.id} - {self.planet_id}>'
+
+    def serialize(self):
+        return {'id': self.id,
+                'planet id': self.planet_id,
+                'user id': self.user_id}
+
 
 class Planets(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -87,3 +136,13 @@ class Planets(db.Model):
     rotation_period = db.Column(db.String(10), nullable=False)
     orbital_period = db.Column(db.String(10), nullable=False)
     gravity = db.Column(db.String(30), nullable=False)
+    def __repr__(self):
+            return f'<Character: {self.id} - {self.name}>'
+
+    def serialize(self):
+        return {'id': self.id,
+                'name': self.name,
+                'diameter': self.diameter,
+                'rotation period': self.rotation_period,
+                'orbital period': self.orbital_period,
+                'gravity ': self.gravity}
--- src/api/routes.py
+++ src/api/routes.py
@@ -1,17 +1,164 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
-from flask import Flask, request, jsonify, url_for, Blueprint
+from flask import Flask, request,requests, jsonify, url_for, Blueprint
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
-from api.models import db, Users
+from api.models import db, Users, Products
+
 
 api = Blueprint('api', __name__)
-CORS(api) # Allow CORS requests to this API
+CORS(api)  # Allow CORS requests to this API
 
 
 @api.route('/hello', methods=['POST', 'GET'])
 def handle_hello():
     response_body = {}
-    response_body["message"] = "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
+    response_body['message'] = "Hello! I'm a message that came from the backend"
     return response_body, 200
+
+# Lista de USUARIOS 
+@api.route('/users', methods=['GET'])
+def users():
+    response_body = {}
+    rows = db.session.execute(db.select(Users)).scalars()
+    print(rows)
+    # opción 1: standard
+    # results = []
+    # for row in rows:
+    #    results.append(row.serialize())
+    # opcion 2: list comprehension
+    # variable = [ target for individual in iterable ]
+    results = [ row.serialize() for row in rows]
+    response_body['message'] = f'Listado de Usuarios'
+    response_body['results'] = results
+    return response_body, 200
+
+# Listar todos los favoritos que pertenecen al usuario actual.
+@api.route('/users/<int:user_id>/favorites', methods=['GET'])
+def user_favorites(user_id):
+    response_body = {}
+    rows = db.session.execute(db.select(user_favorites)).scalars()
+    print(rows)
+    results = [ row.serialize() for row in rows]
+    response_body['message'] = f'Listado de characters'
+    response_body['results'] = results
+    return response_body, 200
+
+# Lista de PERSONAJES 
+@api.route('/characters', methods=['GET'])
+def characters():
+    response_body = {}
+    rows = db.session.execute(db.select(characters)).scalars()
+    print(rows)
+    results = [ row.serialize() for row in rows]
+    response_body['message'] = f'Listado de characters'
+    response_body['results'] = results
+    return response_body, 200
+
+# Muestra la información de un solo personaje según su id.
+@api.route('/characters/<int:characters_id>', methods=['GET'])
+def character(characters_id):
+    response_body = {}
+    url = 'https://swapi.tech/api/people' ##modificar
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        print(data['results'])
+        response_body['message'] = 'Listado de Personajes'
+        response_body['results'] = data['results']
+        return response_body, 200
+    response_body['message'] = 'algo salió mal'
+    return response_body, 400
+
+# Lista de PLANETAS
+@api.route('/planets', methods=['GET'])
+def planets():
+    response_body = {}
+    rows = db.session.execute(db.select(planets)).scalars()
+    print(rows)
+    results = [ row.serialize() for row in rows]
+    response_body['message'] = f'Listado de planetas'
+    response_body['results'] = results
+    return response_body, 200
+
+# Muestra la información de un solo planeta según su id.
+@api.route('/planet/<int:planets_id>', methods=['GET'])
+def planet(planets_id):
+    response_body = {}
+    url = 'https://swapi.tech/api/planets'   ##modificar
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        print(data['results'])
+        response_body['message'] = 'Listado de Planeta'
+        response_body['results'] = data['results']
+        return response_body, 200
+    response_body['message'] = 'algo salió mal'
+    return response_body, 400
+        
+
+
+
+
+# @api.route('/products', methods=['GET', 'POST'])
+# def products():
+#     response_body = {}
+#     if request.method == 'GET':
+#         rows = db.session.execute(db.select(Products)).scalars()
+#         results = [row.serialize() for row in rows]
+#         response_body['results'] = results
+#         response_body['message'] = f'Respuesta para el método {request.method}'
+#         return response_body, 200
+#     if request.method == 'POST':
+#         data = request.json
+#         # print(data, type(data))
+#         row = Products(name=data['name'],
+#                        description=data.get('description', 'n/a'),
+#                        price=data['price'])
+#         db.session.add(row)
+#         db.session.commit()
+#         response_body['message'] = f'Respuesta para el método {request.method}'
+#         response_body['results'] = row.serialize()
+#         return response_body, 200
+
+
+# @api.route('/products/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+# def product(id):
+#     response_body = {}
+#     # Validar si el id existe
+#     row = db.session.execute(db.select(Products).where(Products.id == id)).scalar()
+#     print(row)
+#     if not row:
+#         response_body['message'] = f'El producto id {id} no existe'
+#         return response_body, 404
+#     # TODO: Validar que el usuario pueda ver, modificar o borrar el producto
+#     if request.method == 'GET':
+#         response_body['results'] = row.serialize()
+#         response_body['message'] = f'Respuesta para el método {request.method} del id: {id}' 
+#         return response_body, 200
+#     if request.method == 'PUT':
+#         data = request.json
+#         print('row', row.serialize())
+#         print('data', data)
+#         row.name = data.get('name')
+#         # foo = data['description'] if data.get('description') else None
+#         """
+#         foo = data.get('description', None)
+#         if foo:
+#             row.description = foo
+#         """
+#         row.description = data.get('description', row.description)
+#         row.price = data['price']
+#         db.session.commit()
+#         response_body['message'] = f'Respuesta para el método {request.method} del id {id}'
+#         response_body['results'] = row.serialize()
+#         return response_body, 200
+#     if request.method == 'DELETE':
+#         # La pregunta es: Borro o dashabilito ?
+#         db.session.delete(row)
+#         db.session.commit()
+#         response_body['message'] = f'Hemos borrado el procuto id {id}'
+#         response_body['results'] = {}
+#         return response_body, 200
+    
